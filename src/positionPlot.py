51ccab95045f25c8266e6220a84d9f80ee76496f
Cam Buss <cam.r.buss@gmail.com>
2018-11-04 01:44:36 -0400

    Added argument to insert file

--- src/positionPlot.py
+++ src/positionPlot.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python3
 
+import sys
 import csv
 import numpy as np
 import matplotlib as plt
 
 fileFolderAndName = '20181101/20181101_202450_540_Auton.tsv'
+
+try:
+    firstArg = sys.argv[1]
+    fileFolderAndName = firstArg
+except:
+    print('No input given, using default file')
 
 with open('../data/' + fileFolderAndName, newline='') as csvfile:
     tsvreader = csv.reader(csvfile, delimiter='\t', quotechar='|')
@@ -13,5 +20,7 @@ with open('../data/' + fileFolderAndName, newline='') as csvfile:
     colCount = len(columnHeaderList)
     rowCount = sum(1 for row in tsvreader)
     print('\n\nNumber of Columns: %i  Number of Rows: %i\n\n' % (colCount, rowCount))
+
+
 
 

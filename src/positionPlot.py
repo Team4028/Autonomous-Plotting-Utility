6946dddacf82c39008f69f7b36ee4715b5c7174e
Cam Buss <cam.r.buss@gmail.com>
2018-11-04 01:32:05 -0400

    Calculate number of rows and columns

--- src/positionPlot.py
+++ src/positionPlot.py
@@ -1,3 +1,17 @@
 #!/usr/bin/env python3
-print("Position Plotting")
+
+import csv
 import numpy as np
+import matplotlib as plt
+
+fileFolderAndName = '20181101/20181101_202450_540_Auton.tsv'
+
+with open('../data/' + fileFolderAndName, newline='') as csvfile:
+    tsvreader = csv.reader(csvfile, delimiter='\t', quotechar='|')
+
+    columnHeaderList = next(tsvreader)
+    colCount = len(columnHeaderList)
+    rowCount = sum(1 for row in tsvreader)
+    print('\n\nNumber of Columns: %i  Number of Rows: %i\n\n' % (colCount, rowCount))
+
+
